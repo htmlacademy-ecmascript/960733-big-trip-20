6abb7e1d0113df61c2d456d2befa0b2b6813494f
Roman Kostenko <kostenko.r.khv@gmail.com>
2023-06-19 11:24:07 +1000

    last fix

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 
 const sortByDate = (eventA, eventB) => dayjs(eventA.startDate).diff(dayjs(eventB.startDate));
-const sortByTime = (eventA, eventB) => dayjs(eventA.endDate).diff(dayjs(eventA.startDate)) - dayjs(eventB.endDate).diff(dayjs(eventB.startDate));
-const sortByPrice = (eventA, eventB) => eventA.price - eventB.price;
+const sortByTime = (eventA, eventB) => dayjs(eventB.endDate).diff(dayjs(eventB.startDate)) - dayjs(eventA.endDate).diff(dayjs(eventA.startDate));
+const sortByPrice = (eventA, eventB) => eventB.price - eventA.price;
 
 export {sortByDate, sortByTime, sortByPrice};
